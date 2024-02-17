9dcb0b84b7fb6fd6db0e86882b188613c4642801
isabelxklee <hi@kleetime.com>
2024-02-16 23:48:59 -0500

    updated test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,5 @@
 import Dated from '../src/Dated.ts';
+import { updateTime } from '../src/updateTime/index.ts';
 
 jest.autoMockOff();
 
@@ -8,22 +9,29 @@ test('should initialize date', () => {
   console.log(date);
 });
 
-test('should create custom date', () => {
-  const date = new Dated({
-    time: '01:45:00',
-    day: 31,
-    month: 2,
-    year: 2024,
-    weekdayFormat: 'medium',
-    monthFormat: 'long',
-  });
-  expect(date.weekdayFormat).not.toBe('short');
+test('should update time for existing date', () => {
+  const date = new Dated();
+  updateTime(date, '12:09:00');
+  expect(date.time).toBe('12:09:00');
   console.log(date);
 });
 
-test('should update time', () => {
-  const date = new Dated();
-  date.setTime('15:23:00');
-  expect(date.time).toBe('15:23:00');
-  console.log(date);
-});
+// test('should create custom date', () => {
+//   const date = new Dated({
+//     time: '01:45:00',
+//     day: 31,
+//     month: 2,
+//     year: 2024,
+//     weekdayFormat: 'medium',
+//     monthFormat: 'long',
+//   });
+//   expect(date.weekdayFormat).not.toBe('short');
+//   console.log(date);
+// });
+
+// test('should update time', () => {
+//   const date = new Dated();
+//   date.setTime('15:23:00');
+//   expect(date.time).toBe('15:23:00');
+//   console.log(date);
+// });
